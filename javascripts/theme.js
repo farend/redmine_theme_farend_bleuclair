9d24794081de5379c1ade67c83bf6e2972d8de40
ishikawa <ishikwa@farend.jp>
2019-07-12 06:53:32 +0000

    Fix the layout of #history .tab-content

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -1,5 +1,22 @@
-window.addEventListener('DOMContentLoaded', function () {
-  document.querySelectorAll('a[href*="activity"][title*=":"]').forEach(function (el) {
-    el.setAttribute("data-absolute-date", el.title);
+function setDiscontinuityClassToTabContent(id) {
+  if ($('#' + id).hasClass('selected')) {
+    if (id === 'tab-notes' || id === 'tab-properties') {
+      $('div#tab-content-history').addClass('discontinuity');
+    } else {
+      $('div#tab-content-history').removeClass('discontinuity');
+    }
+  }
+}
+
+$(function(){
+  $('a[href*="activity"][title*=":"]').each(function(_index, element){
+    $(element).attr("data-absolute-date", element.title);
   });
-})
+
+  $('#history > .tabs a').each(function(){
+    setDiscontinuityClassToTabContent($(this).attr('id'));
+    $(this)[0].addEventListener('click', function(){
+      setDiscontinuityClassToTabContent($(this).attr('id'));
+    });
+  });
+});

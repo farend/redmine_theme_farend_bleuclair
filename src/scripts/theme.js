af000a5f460f90c9d6ab096683e47ef107cd4819
ishikawa999 <remember929@gmail.com>
2020-09-25 07:32:13 +0000

    Fix src/scripts/theme.js

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -9,6 +9,16 @@ function setDiscontinuityClassToTabContent(id) {
 }
 
 $(function(){
+  /* Change to open external link in another tab */
+  $("a.external").attr("target","_blank");
+  $("a.help").attr("target","_blank");
+  $("div#footer a[href^='https://www.redmine.org/']").attr("target","_blank");
+
+  if (($("a.help").text() == "ヘルプ" ? "ja" : $("html").attr("lang")) == "ja") {
+    /* When the language is Japanese, the link destination of help is Redmine.jp */
+    $("a.help").attr("href", "http://guide.redmine.jp/");
+  }
+
   $('a[href*="activity"][title*=":"]').each(function(_index, element){
     $(element).attr("data-absolute-date", element.title);
   });

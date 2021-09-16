9ec8e9e5078e05e81844d80b52ed6619ab7d72bf
ishikawa999 <remember929@gmail.com>
2021-09-16 04:18:50 +0000

    Fix scripts

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -4,26 +4,16 @@
 /***/ 935:
 /***/ (function() {
 
-function setDiscontinuityClassToTabContent(id) {
-  if ($('#' + id).hasClass('selected')) {
-    if (id === 'tab-notes' || id === 'tab-properties') {
-      $('div#tab-content-history').addClass('discontinuity');
-    } else {
-      $('div#tab-content-history').removeClass('discontinuity');
-    }
-  }
-}
-
 $(function () {
-  $('a[href*="activity"][title*=":"]').each(function (_index, element) {
-    $(element).attr("data-absolute-date", element.title);
-  });
-  $('#history > .tabs a').each(function () {
-    setDiscontinuityClassToTabContent($(this).attr('id'));
-    $(this)[0].addEventListener('click', function () {
-      setDiscontinuityClassToTabContent($(this).attr('id'));
-    });
-  });
+  /* Change to open external link in another tab */
+  $("a.external").attr("target", "_blank");
+  $("a.help").attr("target", "_blank");
+  $("div#footer a[href^='https://www.redmine.org/']").attr("target", "_blank");
+
+  if (($("a.help").text() == "ヘルプ" ? "ja" : $("html").attr("lang")) == "ja") {
+    /* When the language is Japanese, the link destination of help is Redmine.jp */
+    $("a.help").attr("href", "http://guide.redmine.jp/");
+  }
 });
 
 /***/ })
--- src/scripts/bleuclair.js
+++ src/scripts/bleuclair.js
@@ -1,22 +1,11 @@
-function setDiscontinuityClassToTabContent(id) {
-  if ($('#' + id).hasClass('selected')) {
-    if (id === 'tab-notes' || id === 'tab-properties') {
-      $('div#tab-content-history').addClass('discontinuity');
-    } else {
-      $('div#tab-content-history').removeClass('discontinuity');
-    }
-  }
-}
-
 $(function(){
-  $('a[href*="activity"][title*=":"]').each(function(_index, element){
-    $(element).attr("data-absolute-date", element.title);
-  });
+  /* Change to open external link in another tab */
+  $("a.external").attr("target","_blank");
+  $("a.help").attr("target","_blank");
+  $("div#footer a[href^='https://www.redmine.org/']").attr("target","_blank");
 
-  $('#history > .tabs a').each(function(){
-    setDiscontinuityClassToTabContent($(this).attr('id'));
-    $(this)[0].addEventListener('click', function(){
-      setDiscontinuityClassToTabContent($(this).attr('id'));
-    });
-  });
+  if (($("a.help").text() == "ヘルプ" ? "ja" : $("html").attr("lang")) == "ja") {
+    /* When the language is Japanese, the link destination of help is Redmine.jp */
+    $("a.help").attr("href", "http://guide.redmine.jp/");
+  }
 });

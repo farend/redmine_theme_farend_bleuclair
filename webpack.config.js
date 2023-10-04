389d8bba25eda89ad449462bf3357b857efea631
Daniel Kastl <daniel@georepublic.de>
2023-10-04 15:51:38 +0900

    Adds support for fonts and images to webpack Upgrades FontAwsome to v6
    Signed-off-by: Daniel Kastl <daniel@georepublic.de>

--- webpack.config.js
+++ webpack.config.js
@@ -6,15 +6,19 @@
  *
  * CSS files in src/styles will be compiled to the following directory:
  *   stylesheets/theme.css
+ *
+ * Webfonts from node_modules will be copied to the following directory:
+ *   stylesheets/webfonts
+ *
+ * Images from src/images will be copied to the following directory:
+ *   stylesheets/images
+ *
+ * SVG images from src/images will be embedded in the CSS file if they are less 10 KB.
  */
 
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-
-// Define output directories
-const scriptsOutputDir = 'javascripts';
-const stylesOutputDir = 'stylesheets';
 
 // Define rules for compiling JavaScript
 const jsRule = {
@@ -37,7 +41,7 @@ const scssRule = {
       loader: 'css-loader',
       options: {
         sourceMap: true,
-        url: false
+        url: true
       },
     },
     {
@@ -59,22 +63,60 @@ const scssRule = {
   ],
 };
 
+// Define rules for copying fonts
+const fontRule = {
+  test: /\.(woff2|ttf|woff)$/i,
+  type: 'asset/resource',
+  generator: {
+    filename: 'webfonts/[name][ext]',
+  },
+};
+
+// Define rules for handling images
+const imageRule = {
+  test: /\.(png|jpe?g|gif)$/i,  // Exclude svg from this rule
+  type: 'asset/resource',
+  generator: {
+    filename: 'images/[name][ext]',
+  },
+};
+
+// Define rules for embedding SVG images (< 10 KB)
+const svgRule = {
+  test: /\.svg$/i,
+  type: 'asset',
+  parser: {
+    dataUrlCondition: {
+      maxSize: 10 * 1024, // 10 KB
+    },
+  },
+  generator: {
+    filename: 'images/[name][ext]',
+  },
+};
+
 module.exports = {
   target: ['web', 'es5'],
   entry: './src/index.js',
   devtool: 'source-map',
   output: {
-    path: path.resolve(__dirname, scriptsOutputDir),
-    filename: 'theme.js'
+    path: path.resolve(__dirname, './stylesheets'),
+    filename: '../javascripts/theme.js',
+    publicPath: '',
   },
   module: {
     rules: [
       jsRule,
       scssRule,
+      fontRule,
+      imageRule,
+      svgRule,
     ],
   },
   plugins:[
-    new MiniCssExtractPlugin({ filename: `../${stylesOutputDir}/theme.css` }),
+    new MiniCssExtractPlugin({
+      filename: `theme.css`,
+    }),
   ],
   optimization: {
     minimizer: [new CssMinimizerPlugin({
